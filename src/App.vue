c817ebe071b76b0988b0e819f466fdf085108729
Hugo <hugo@MacBook-Pro-de-Hugo.local>
2019-06-12 13:41:25 +0200

    setup router and translations

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">{{ $t('nav.home') }}</router-link> |
+      <router-link to="/about">{{ $t('nav.about') }}</router-link>
     </div>
+    <LocaleSwitch/>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  import LocaleSwitch from "./components/LocaleSwitch";
+  import * as locales from "./constants/locales";
+
+  export default {
+    components: {LocaleSwitch},
+    /**
+     *
+     */
+    beforeCreate() {
+      let locale = localStorage.getItem('diktat-locale');
+      if (!locale) locale = locales.DEFAULT_LOCALE;
+
+      this.$i18n.locale = locale;
     }
   }
-}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
