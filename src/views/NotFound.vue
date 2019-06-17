316123f48cea58ae4b87b62369e024b10d3461fc
Hugo <hugo@MacBook-Pro-de-Hugo.local>
2019-06-17 10:06:34 +0200

    add Modal component

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,12 +2,25 @@
     <div>
         <h1>{{$t('NotFound.title')}}</h1>
         <p>{{$t('NotFound.content')}}</p>
+        <button @click="showModal = !showModal">Show Modal</button>
+        <Modal :show="showModal">
+            <h2>Modal test</h2>
+            <p>Modal test for the 404 page.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, asperiores assumenda atque eaque, eveniet ex illum minima optio praesentium quam sapiente tenetur vitae. Culpa eaque in laboriosam minima qui, totam?</p>
+        </Modal>
     </div>
 </template>
 
 <script>
+    import Modal from "../components/Modal";
     export default {
-        name: "NotFound"
+        name: "NotFound",
+        components: {Modal},
+        data() {
+            return {
+                showModal: false
+            }
+        }
     }
 </script>
 
