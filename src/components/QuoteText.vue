3cc7bd705453653af772c5278ad2d81c397691f1
Loris <devloris@gmail.com>
2019-06-17 15:05:32 +0200

    Landing page

---
+++ src/components/QuoteText.vue
@@ -0,0 +1,51 @@
+<template>
+    <FullscreenFrame>
+        <blockquote>
+            <p>{{ text }}</p>
+            <footer>— <cite>{{ source }}</cite></footer>
+        </blockquote>
+    </FullscreenFrame>
+</template>
+
+<script>
+    import FullscreenFrame from "./FullscreenFrame";
+    export default {
+        name: "QuoteText",
+        components: {FullscreenFrame},
+        props: {
+            text: '',
+            source: '',
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    blockquote {
+        max-width: 600px;
+        padding: 0 10px;
+        p {
+            position: relative;
+
+            &:before {
+                content: '“';
+                opacity: 0.6;
+            }
+            &:after {
+                content: '”';
+                transform: scale(4, 4);
+                opacity: 0.6;
+            }
+        }
+    }
+
+    footer {
+        font-size: 20px;
+    }
+
+    cite  {
+        opacity: 0.6;
+        font-weight: normal;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
-    <h1>{{$t('home.title')}}</h1>
+    <FullscreenFrame>
+      <Logo></Logo>
+    </FullscreenFrame>
+    <QuoteText :text="$t('definition.text')" :source="$t('definition.source')"></QuoteText>
+    <CenteredText :text="$t('but.text')"></CenteredText>
+    <CenteredText :text="$t('yousee.text')"></CenteredText>
+    <CenteredText :text="$t('look.text')"></CenteredText>
   </div>
 </template>
 
 <script>
+import FullscreenFrame from "../components/FullscreenFrame";
+import QuoteText from "../components/QuoteText";
+import CenteredText from "../components/CenteredText";
+import Logo from "../components/Logo";
 export default {
   name: 'home',
+  components: {Logo, CenteredText, QuoteText, FullscreenFrame},
 }
 </script>
