ebec87a826d2c445e367abb5643c15917af77139
Hugo <hugo@MacBook-Pro-de-Hugo.local>
2019-06-17 07:25:10 +0200

    add 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{$t('NotFound.title')}}</h1>
+        <p>{{$t('NotFound.content')}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
