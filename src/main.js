d6db5c17c373829b8cfee765892ead35b4fe89b1
Loris <devloris@gmail.com>
2019-06-20 08:36:05 +0200

    update website

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import lax from 'lax.js'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$lax = lax;
 new Vue({
   router,
   i18n,
